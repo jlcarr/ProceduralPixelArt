d058bc39860b14bb5dc4aede3080b05cf036ffd6
Joseph Carr <joseph.carr@mail.mcgill.ca>
2021-05-08 13:55:04 -0400

    Added thin staircases

--- building_blocks.py
+++ building_blocks.py
@@ -248,6 +248,80 @@ def create_staircase_platform(x, l, lr=1, fb=1):
 
 
 
+def create_staircase_thin(x, l, lr=1, fb=1):
+	img_h = 4 * x + 5
+	img_w = 4 * x + 5
+	image_obj = Image.new('RGBA', (img_w,img_h), color=(0,0,0,0))
+	draw_obj = ImageDraw.Draw(image_obj)
+	
+	# Orientation constants
+	x_back = (1-lr)*(2*x+2) + lr*(1-fb)*(x+1)
+	y_back = 3*x+3 - (1-fb)*(x+1)/2
+	x_front = 2*x+2 + lr*(1-fb)*(x+1)
+	y_front = 4*x+4 - (1-fb)*(x+1)/2
+	# Final step constants
+	i_final = x//l -1
+	remainder = x+1 - l*(i_final+1)
+	
+	if fb > 0:
+		#Back-Top exterior
+		draw_obj.line([(x_front, 0), ((1+lr)*(2*x+2), x+1)][::lr], fill=(0,0,0,255))
+	
+		# Front-Bottom exterior
+		if lr > 0:
+			draw_obj.line([(0, 3*x+3), (2*x+2, 4*x+4), (4*x+4, x+1)], fill=(0,0,0,255))
+		else:
+			draw_obj.line([(0, x+1), (2*x+2, 4*x+4), (4*x+4, 3*x+3)], fill=(0,0,0,255))
+
+		#Back
+		for i in range(x//l):
+			# Back, horizontal edge
+			draw_obj.line([(x_back + fb*lr*2*l*i, y_back - l*((2+fb)*i+2)), (x_back + fb*lr*2*l*(i+1), y_back - (2+fb)*l*(i+1))][::fb*lr], fill=(0,0,0,255))
+			# Back, vertical
+			draw_obj.line([(x_back + fb*lr*2*l*i, y_back - l*((2+fb)*i+2)), (x_back + fb*lr*2*l*i, y_back - (2+fb)*l*i)], fill=(0,0,0,255))
+		# Back final step
+		draw_obj.line([(x_back + fb*lr*2*l*(i_final+1), y_back - (2+fb)*l*(i_final+1)), (x_back + fb*lr*2*l*(i_final+1), (1-fb)*(x+1)/2 + fb*remainder), (2*x+2 - lr*(1-fb)*(x+1), (1-fb)*(x+1)/2)][::fb*lr], fill=(0,0,0,255))
+
+		#Fill the color by the hull, right in the middle
+		ImageDraw.floodfill(image_obj,(2*x+2, 2*x+2), (255,255,255,255))
+		
+		for i in range(x//l):
+			# Concave edge
+			draw_obj.line([(x_back + lr*2*l*(i+1), y_back - 3*l*(i+1)), (x_front + lr*2*l*(i+1), y_front - 3*l*(i+1))][::fb*lr], fill=(0,0,0,255))
+	else:
+		# Back final step
+		draw_obj.line([(x_back + fb*lr*2*l*(i_final+1), (1-fb)*(x+1)/2 + fb*remainder), (2*x+2 - lr*(1-fb)*(x+1), (1-fb)*(x+1)/2)][::fb*lr], fill=(0,0,0,255))
+		draw_obj.line([(x_back + fb*lr*2*l*(i_final+1) - 2*fb*lr*remainder, y_back - (2+fb)*l*(i_final+1) + fb*remainder), (x_back + fb*lr*2*l*(i_final+1), (1-fb)*(x+1)/2 + fb*remainder)][::fb*lr], fill=(0,0,0,255))
+		# Back second final step
+		draw_obj.line([(x_back + fb*lr*2*l*i_final, y_back - l*((2+fb)*i_final+2)), (x_back + fb*lr*2*l*(i_final+1) - 2*fb*lr*remainder, y_back - (2+fb)*l*(i_final+1) + fb*remainder)][::fb*lr], fill=(0,0,0,255))
+
+	for i in range(x//l):
+		# Convex edge
+		draw_obj.line([(x_back + fb*lr*2*l*i, y_back - l*((2+fb)*i+2)), (x_front + fb*lr*2*l*i, y_front - l*((2+fb)*i+2))][::lr], fill=(0,0,0,255))
+
+	if fb < 0:
+		# Front-Bottom exterior
+		draw_obj.line([((1-lr)*(2*x+2), x+1), ((1+lr)*(2*x+2), 3*x+3)][::lr], fill=(0,0,0,255))
+
+		# Fill from center
+		ImageDraw.floodfill(image_obj,(2*x+2, 2*x+1), (255,255,255,255))
+
+	# Convex edge, final step
+	draw_obj.line([(x_back + fb*lr*2*l*(i_final+1), (1-fb)*(x+1)/2 + fb*remainder), (x_front + fb*lr*2*l*(i_final+1), (3-fb)*(x+1)/2 + fb*remainder)][::lr], fill=(0,0,0,255)) #(2*x+2 - lr*(1-fb)*(x+1), (1-fb)*(x+1)/2)
+
+	for i in range(x//l):
+		# Front, horizontal edge
+		draw_obj.line([(x_front + fb*lr*2*l*i, y_front - l*((2+fb)*i+2)), (x_front + fb*lr*2*l*(i+1), y_front - (2+fb)*l*(i+1))][::fb*lr], fill=(0,0,0,255))
+		# Front, vertical
+		draw_obj.line([(x_front + fb*lr*2*l*i, y_front - l*((2+fb)*i+2)), (x_front + fb*lr*2*l*i, y_front - (2+fb)*l*i)], fill=(0,0,0,255))
+	# Front, final step
+	draw_obj.line([(x_front + fb*lr*2*l*(i_final+1), y_front - (2+fb)*l*(i_final+1)), (x_front + fb*lr*2*l*(i_final+1), (3-fb)*(x+1)/2 + fb*remainder), ((2+lr+lr*fb)*(x+1), (3-fb)*(x+1)/2)][::fb*lr], fill=(0,0,0,255))
+
+	del draw_obj
+	return image_obj
+
+
+
 
 def create_brick_cube(x, w_sep, h_sep):
 	# Create a base
@@ -402,6 +476,11 @@ if __name__ == "__main__":
 	create_staircase_platform(x, l, fb=-1,lr=1).save("./images/staircase_platform_rf_sprite.png")
 	create_staircase_platform(x, l, fb=1,lr=-1).save("./images/staircase_platform_lb_sprite.png")
 	create_staircase_platform(x, l, fb=-1,lr=-1).save("./images/staircase_platform_lf_sprite.png")
+
+	create_staircase_thin(x, l, fb=1,lr=1).save("./images/staircase_thin_rb_sprite.png")
+	create_staircase_thin(x, l, fb=-1,lr=1).save("./images/staircase_thin_rf_sprite.png")
+	create_staircase_thin(x, l, fb=1,lr=-1).save("./images/staircase_thin_lb_sprite.png")
+	create_staircase_thin(x, l, fb=-1,lr=-1).save("./images/staircase_thin_lf_sprite.png")
 
 	x = 19
 	w_sep = 16
